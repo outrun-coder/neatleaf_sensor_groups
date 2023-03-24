cc86778b2478b9b36455d3cbd4734642723cc2d2
David Pineda <david.pineda.orvc@gmail.com>
2023-03-23 20:04:56 -0400

    Block and style controls form

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,8 +1,23 @@
+import { Form } from "react-bootstrap";
 
 
 const Controls = () => {
   return (
-    <h1>START Controls</h1>
+    <Form>
+      <Form.Group style={{display: 'Flex', flexDirection: 'column', padding: '20px 60px'}}>
+        <Form.Label style={{marginBottom: '20px'}}>
+          Controls
+        </Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Search..."
+          style={{marginBottom: '20px'}}/>
+        <Form.Check
+          type="checkbox"
+          label="Only show nominal measurments"
+          style={{marginBottom: '20px'}}/>
+      </Form.Group>
+    </Form>
   );
 }
 
