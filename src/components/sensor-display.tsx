554206ac177eded6b65b3f9b7e23de7dc53a558f
David Pineda <david.pineda.orvc@gmail.com>
2023-03-23 20:32:11 -0400

    block and style sensor measurement display

--- src/components/sensor-display.tsx
+++ src/components/sensor-display.tsx
@@ -2,7 +2,22 @@
 
 const SensorDisplay = () => {
   return (
-    <h1>START Sensor Display</h1>
+    <div className="sensor-display">
+      <h1>Sensor Measurements</h1>
+
+      <div className="greenhouse-status">
+        <div className="greenhouse-label">Greenhouse label</div>
+
+        <ul className="measurement-list">
+          <li className="sensor-measurement">
+              <span className="sensor-label">Sensor Label</span>
+              <span className="sensor-reading">Sensor Reading</span>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
   );
 }
 
